988f8fc335ef1d79006d414c49f2223dd8d23387
Ben Malburg <bamalburg@gmail.com>
2020-03-24 20:22:37 -0500

    Groovy file to load data into graph

---
+++ Data/loadAirRoutes.groovy
@@ -0,0 +1,21 @@
+graph = JanusGraphFactory.open('inmemory')
+
+// Change the path below to point to wherever you put the graphml file
+graph.io(graphml()).readGraph('C:/Users/BenjaminMalburg/Documents/GitHub/graph/sample-data/air-routes-latest.graphml')
+mgmt = graph.openManagement()
+
+g=graph.traversal()
+:set max-iteration 1000
+
+println "Vertex Labels:"
+mgmt.getVertexLabels()
+
+println "Edge Labels:"
+mgmt.getRelationTypes(EdgeLabel.class)
+
+println "Property Keys:"
+types = mgmt.getRelationTypes(PropertyKey.class)
+
+println "Data Types and Cardinality of Properties:"
+types.each{println "$it\t: " + mgmt.getPropertyKey("$it").dataType() + " " + mgmt.getPropertyKey("$it").cardinality()}
+mgmt.commit()
